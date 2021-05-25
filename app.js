acc13f2e296c2e7b65ce8fde614355b3e6a5186e
Arthur Della Faille <arthur16.dellafaille@gmail.com>
2021-05-25 12:54:34 +0200

    user setup

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Sauce = require('./models/sauce');
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user')
 
 mongoose.connect('mongodb+srv://arthur_16:AlphaRomeo16@cluster0.srb2i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -21,38 +22,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/sauce', (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce({
-        ...req.body
-    });
-    sauce.save()
-        .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
-        .catch(() => res.status(400).json({ error }));
-});
-
-app.put('/api/sauce/:id', (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.statuts(200).json({ message: 'Sauce modifiée !'}))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/sauce/:id', (req, res, next) =>{
-    Sauce.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/sauce/:id', (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/sauce', (req, res, next) => {
-    Sauce.find()
-        .then(sauces => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/sauce', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
