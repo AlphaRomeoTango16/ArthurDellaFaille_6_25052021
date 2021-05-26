fd43c361236397ff1c53e9d81a5cc5fd04814e11
Arthur Della Faille <arthur16.dellafaille@gmail.com>
2021-05-26 16:12:51 +0200

    security

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -52,17 +52,24 @@ exports.getAllSauce = (req, res, next) => {
 }
 
 exports.likeSauce = (req, res, next) => {
+    const userId = req.body.userId
+    const sauceLike = req.body.likes;
+    const sauceDislike = req.body.dislikes;
+    const usersLiked = req.body.usersLiked;
+    const usersDisliked = req.body.usersDisliked;
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
-            const likeObject = sauce.usersLiked;
-            const dislikeObject = sauce.userDisliked;
-            if (req.body.like == +1) {
-                likeObject.push(userId);
-            } else if (req.body.like == -1) {
-                dislikeObject.push(userId);
-            } else if (req.body.like == 0) {
-                likeObject.splice(1, 1);
-                dislikeObject.splice(1, 1);
+            if (sauceLike == +1) {
+                usersLiked.push(userId);
+                sauceLike.inc(+1);
+            } else if (sauceLike == -1) {
+                usersDisliked.push(userId);
+                sauceDislike.inc(-1);
+            } else if (sauceLike == 0) {
+                usersLiked.push(userId);
+                usersDisliked.push(userId);
+                sauceLike.inc(0);
+                sauceDislike.inc(0);
             }
         })
         .catch(error => res.status(500).json({ error }));
